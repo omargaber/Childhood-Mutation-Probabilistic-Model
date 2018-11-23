8a1a12af60ba6ebd5af40c1324e3658e2db2e8b5
Omar Gaber <omargaber956@gmail.com>
2018-11-23 05:52:54 +0200

    Poisson

--- project.py
+++ project.py
@@ -1,45 +1,46 @@
 import math
+import matplotlib.pyplot as plt
 
 mean_tumors_infinity = 3.0
 mean_tumors_infinity_true = 4.0
 
 # Data
 # Number of sample size
-unilateral_familial = {'age_one': 4.0, 'age_two': 6.0, 'age_three': 8.0,
-                       'age_four': 4.0, 'age_five': 2.0, 'age_older': 0}
+unilateral_familial = {'1': 4.0, '2': 6.0, '3': 8.0,
+                       '4': 4.0, '5': 2.0, '6': 0}
 
-bilateral_familial = {'age_one': 45.0, 'age_two': 29.0, 'age_three': 11.0,
-                      'age_four': 2.0, 'age_five': 1.0, 'age_older': 2.0}
+bilateral_familial = {'1': 45.0, '2': 29.0, '3': 11.0,
+                      '4': 2.0, '5': 1.0, '6': 2.0}
 
-unilateral_non_familial = {'age_one': 33.0, 'age_two': 39.0, 'age_three': 40.0,
-                           'age_four': 20.0, 'age_five': 11.0, 'age_older': 6.0}
+unilateral_non_familial = {'1': 33.0, '2': 39.0, '3': 40.0,
+                           '4': 20.0, '5': 11.0, '6': 6.0}
 
 
 # Calculated results to be compared with predicted
-hc_calc_at_infinity = {'age_one': 1.0, 'age_two': 0.62, 'age_three': 0.33,
-                       'age_four': 0.13, 'age_five': 0.051, 'age_older': 0.014}
+hc_calc_at_infinity = {'1': 1.0, '2': 0.62, '3': 0.33,
+                       '4': 0.13, '5': 0.051, '6': 0.014}
 
-hc_calc_at_infinity_true = {'age_one': 1.0, 'age_two': 0.58, 'age_three': 0.27,
-                            'age_four': 0.10, 'age_five': 0.045, 'age_older': 0.017}
+hc_calc_at_infinity_true = {'1': 1.0, '2': 0.58, '3': 0.27,
+                            '4': 0.10, '5': 0.045, '6': 0.017}
 
-hu_calc_at_infinity = {'age_one': 1.0, 'age_two': 0.74, 'age_three': 0.49,
-                       'age_four': 0.24, 'age_five': 0.12, 'age_older': 0.037}
+hu_calc_at_infinity = {'1': 1.0, '2': 0.74, '3': 0.49,
+                       '4': 0.24, '5': 0.12, '6': 0.037}
 
-hu_calc_at_infinity_true = {'age_one': 1.0, 'age_two': 0.73, 'age_three': 0.46,
-                            'age_four': 0.24, 'age_five': 0.13, 'age_older': 0.037}
+hu_calc_at_infinity_true = {'1': 1.0, '2': 0.73, '3': 0.46,
+                            '4': 0.24, '5': 0.13, '6': 0.037}
 
-hb_calc_at_infinity = {'age_one': 1.0, 'age_two': 0.55, 'age_three': 0.24,
-                       'age_four': 0.060, 'age_five': 0.014, 'age_older': 0.0013}
+hb_calc_at_infinity = {'1': 1.0, '2': 0.55, '3': 0.24,
+                       '4': 0.060, '5': 0.014, '6': 0.0013}
 
-hb_calc_at_infinity_true = {'age_one': 1.0, 'age_two': 0.53, 'age_three': 0.22,
-                            'age_four': 0.058, 'age_five': 0.017, 'age_older': 0.0034}
+hb_calc_at_infinity_true = {'1': 1.0, '2': 0.53, '3': 0.22,
+                            '4': 0.058, '5': 0.017, '6': 0.0034}
 
 
-m_t = {'age_one': 0.0, 'age_two': 0.0, 'age_three': 0.0,
-       'age_four': 0.0, 'age_five': 0.0, 'age_older': 0.0}
+m_t = {'1': 0.0, '2': 0.0, '3': 0.0,
+       '4': 0.0, '5': 0.0, '6': 0.0}
 
-m_t_i = {'age_one': 0.0, 'age_two': 0.0, 'age_three': 0.0,
-         'age_four': 0.0, 'age_five': 0.0, 'age_older': 0.0}
+m_t_i = {'1': 0.0, '2': 0.0, '3': 0.0,
+         '4': 0.0, '5': 0.0, '6': 0.0}
 
 
 for i in hc_calc_at_infinity:
@@ -62,13 +63,13 @@ for i in hc_calc_at_infinity_true:
     #    ((1 - math.exp(mean_tumors_infinity)) * hc_calc_at_infinity[i]))) * -1
     m_t_i[i] = final
 
-print("When m(t) = 3")
-for i in m_t:
-    print i, m_t[i]
+# print("When m(t) = 3")
+# for i in m_t:
+#     print i, m_t[i]
 
-print("\nWhen m(t) = 4")
-for i in m_t_i:
-    print i, m_t_i[i]
+# print("\nWhen m(t) = 4")
+# for i in m_t_i:
+#     print i, m_t_i[i]
 
 
 # Functions
@@ -138,3 +139,20 @@ for i in hb_calc_at_infinity:
 
 for i in hb_calc_at_infinity_true:
     hb_poisson_infinity_true[i] = HB(m_t_i[i], mean_tumors_infinity_true)
+
+
+print("When m(t) = 3\nHereditary Unilateral calculated")
+for i in hu_calc_at_infinity:
+    print i, ": ", hu_poisson_infinity[i]
+
+print("\nWhen m(t) = 4\nHereditary Unilateral calculated")
+for i in hu_calc_at_infinity_true:
+    print i, ": ", hu_poisson_infinity_true[i]
+print("-----------------------------------")
+print("\nWhen m(t) = 3\nHereditary Bilateral calculated")
+for i in hb_calc_at_infinity:
+    print i, ": ", hb_poisson_infinity[i]
+
+print("\nWhen m(t) = 4\nHereditary Bilateral calculated")
+for i in hb_calc_at_infinity_true:
+    print i, ": ", hb_poisson_infinity_true[i]
